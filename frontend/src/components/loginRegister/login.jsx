0d4dec6964555c8c82d8f4b7d28b7d7cf77129b4
Rafael acevedo <Racevedo@ulearning.com.co>
2023-09-12 17:04:04 -0500

    register

--- frontend/src/components/loginRegister/login.jsx
+++ frontend/src/components/loginRegister/login.jsx
@@ -1,5 +1,5 @@
 import{Bienvenido, Boton, Cajainpu, Checkbox, Contaimput, Forgot, Imgwanto, Input, Principal, Remenber, Wanto,} from "./styles/styledLogin"
-import wanto from "../asset/wanto_Mesa de trabajo 1.png"
+import wanto from "../asset/Wanto.svg"
 
 const Login = () => {
   return(
@@ -15,10 +15,10 @@ const Login = () => {
           <Input type="password" placeholder="Contraseña"/>
           <Forgot><h5>¿Forgot Password?</h5></Forgot>
           <Remenber>
-            <Checkbox type="checkbox"/>
+            <Checkbox type="checkbox" />    
             <h6>Remenber Password</h6>
           </Remenber>
-          <Boton><h2>Login</h2></Boton>
+          <Boton><h2>Sign In</h2></Boton>
           <h6 style={{marginTop:"10px",cursor: "pointer"}}>¿don´t have account?</h6>
         </Cajainpu>
       </Contaimput>
---
+++ frontend/src/components/loginRegister/styles/styledRegister.jsx
@@ -0,0 +1,132 @@
+import styled from "styled-components";
+import blanco from"../../asset/blanco.avif"
+
+export const Principal = styled.section`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  background-image: url(${blanco});
+  background-size: cover;
+  background-repeat: no-repeat; 
+ /*  background-color: #f30c0c; */
+`;
+
+export const Bienvenido = styled.section`
+  background: rgb(0,212,255);
+background: linear-gradient(90deg, rgba(0,212,255,1) 0%, rgba(36,32,119,1) 100%);
+  width: 100%;
+  height: 12%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 40px;
+
+  h1{
+    color: #fff;
+  }
+`;
+
+export const Wanto = styled.div`
+  /* background-color: red; */
+  width: 100%;
+  height: 20%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+export const Imgwanto = styled.img`
+width: 30%;
+height: 80%;
+`;
+
+export const Contaimput = styled.div`
+  /* background-color:#000; */
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  height: 60%;
+`;
+
+export const Cajainpu = styled.div`
+  background-color: #ffffff3b;
+  box-shadow: 0px 3px 3px 3px #cecdcd88;
+  display: flex;
+  align-items: center;
+ /*  justify-content:space-between; */
+  padding: 1%;
+  flex-direction: column;
+  box-sizing: border-box;
+  width: 25%;
+  height: 100%;
+  border-radius: 20px;
+  &:hover{
+    box-shadow: 0px 5px 5px 5px #808080;
+    transition: all 0.8s ease;
+  }
+`
+
+export const Input = styled.input`
+  width: 70%;
+  height: 7%;
+  border-radius: 7px;
+  margin-top: 20px;
+  border: 2px solid #00000036;
+  outline-color: rgb(0,212,255);
+  border-color: #242077;
+  padding: 2px;
+  &:hover{
+    border-color: rgb(0,212,255);
+  }
+`;
+
+/* export const Forgot = styled.div`
+background-color: red; 
+  width: 71%;
+  height: 5%;
+  margin-top: 5px;
+  h5{
+    color: #000;
+    display: flex;
+    justify-content: right;
+    font-size: 10px;
+    font-weight: 600;
+    cursor: pointer;
+  }
+` */
+/* export const Remenber = styled.div`
+  background-color: red; 
+  width: 71%;
+  height: 5%;
+  margin-top: 5px;
+  display: flex;
+  align-items: center;
+  margin-bottom: 30px;
+  H6{
+    font-weight: 600
+  }
+` */
+/* export const Checkbox = styled.input`
+  width: 10%;
+  height: 70%;
+  
+`; */
+
+export const Boton = styled.button`
+  background-color: #242077;
+  height: 10%;
+  width: 50%;
+  cursor: pointer;
+  border-radius: 15px;
+  margin-top: 40px;
+  border: none; 
+  H2{
+    color: white;
+    font-weight: 500
+  }
+  &:hover{
+    background-color: #26B2FB;
+    transition: 1s;
+  }
+`
+
