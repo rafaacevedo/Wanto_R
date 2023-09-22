e514ccd4129d1c85053088301db58a4eb5f429d9
Rafael acevedo <Racevedo@ulearning.com.co>
2023-09-22 16:55:18 -0500

    haciendo el register

--- frontend/src/components/loginRegister/register.jsx
+++ frontend/src/components/loginRegister/register.jsx
@@ -1,14 +1,22 @@
 import{Bienvenido, Boton, Cajainpu, Imgwanto, Input, Principal,  Wanto,} from "./styles/styledRegister"
 import wanto from "../asset/Wanto.svg"
 import { FiChevronLeft } from "react-icons/fi";
+import { useState } from "react";
 
 const Register = () => {
+  const [ email, setEmail ] = useState( "" );
+  const [ password, setPassword ] = useState("");
+  const [name, setFirstName] = useState("")
+  const [lastname, setLastName] = useState("")
+  const [number, setNumber] = useState("")
+
     const cone = () => {
         try {
             (window.location.href ="http://localhost:5173/login");
         } catch (error) {
             alert(error)
     }
+
   }
   return(
     <Principal>
@@ -19,12 +27,39 @@ const Register = () => {
         <Cajainpu>
           <form action="">
             <p className="register">Register</p>
-            <Input type="text" placeholder="First Name"/>
-            <Input type="text" placeholder="Last Name"/>
-            <Input type="number" placeholder="Phone Number "/>
-            <Input type="text" placeholder="Email address"/>
-            <Input type="password" placeholder="Password"/>
-            <Input type="password" placeholder="confirm Password"/>
+            <Input type="text"
+            placeholder="First Name"
+            value={name}
+            onChange={(e) => setFirstName(e.target.value)}
+            required/>
+
+            <Input type="text"
+            placeholder="Last Name"
+            value={lastname}
+            onChange={(e) => setLastName(e.target.value)}
+            required/>
+
+            <Input type="number"
+            placeholder="Phone Number"
+            value={number}
+            onRateChange={(e) => setNumber(e.target.value)}
+            required/>
+
+            <Input type="email"
+            placeholder="Email address"
+            value={email}
+            onRateChange={(e) => setEmail(e.target.value)}
+            required/>
+
+            <Input type="password" 
+            placeholder="Password"
+            value={password}
+            onRateChange={(e) => setPassword(e.target.value)}
+            required/>
+
+            <Input type="password"
+            placeholder="confirm Password"
+            required/>
             <Boton><h2>Sign Up</h2></Boton>
           </form>
         </Cajainpu>
