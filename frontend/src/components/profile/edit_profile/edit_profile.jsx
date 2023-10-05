f3fb4c3045a424363d4e3d48726dd844b0ebf546
alejandro brinez <abrinez@ulearning.com.co>
2023-10-05 13:24:48 -0500

    Editar perfil ya no permite espacios

--- frontend/src/components/profile/edit_profile/edit_profile.jsx
+++ frontend/src/components/profile/edit_profile/edit_profile.jsx
@@ -1,32 +1,61 @@
-import { Header, Maincontainer} from "../styledProfile";
-import { useEffect, useState }  from "react";
-import { useNavigate }          from 'react-router-dom'
-import { ContainerBody, Tittle1 , Tiltle, FullName, InforUser, Containerimg, ButtUser, InfoUserContainer }   from "./styledEdit_profile";
-import  Wanto                   from "../../asset/Wanto.svg";
-import  Perfilfinal             from "../../asset/perfilfinal.png";
-import { FiChevronLeft }        from "react-icons/fi";
-import { VITE_url_fronten, VITE_url_Backend  }     from "../../home/home";
+import { Header, Maincontainer } from "../styledProfile";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import {
+  ContainerBody,
+  Tittle1,
+  Tiltle,
+  FullName,
+  InforUser,
+  Containerimg,
+  ButtUser,
+  InfoUserContainer,
+} from "./styledEdit_profile";
+import Wanto from "../../asset/Wanto.svg";
+import Perfilfinal from "../../asset/perfilfinal.png";
+import { FiChevronLeft } from "react-icons/fi";
+import { VITE_url_fronten, VITE_url_Backend } from "../../home/home";
 import axios from "axios";
 import swal from "sweetalert";
+
 const Editprofile = () => {
+  const [, setLoading] = useState(true);
+  const [firstName, setFirtsName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [telefono, setTelefono] = useState("");
 
-    const [, setLoading ] = useState( true );
-    const [firstName, setFirtsName] = useState( '' );
-    const [lastName, setLastName] = useState  ( '' );
-    const [email, setEmail] = useState        ( '' );
-    const [telefono, setTelefono] = useState  ( '' );
-    
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
 
-    const Act =  async ( e ) => {
+    // Verificar si el valor incluye la tecla de espacio y evitar su ingreso
+    if (value.includes(" ")) {
+      return;
+    }
+
+    switch (name) {
+      case "firstName":
+        setFirtsName(value);
+        break;
+      case "lastName":
+        setLastName(value);
+        break;
+      case "email":
+        setEmail(value);
+        break;
+      case "telefono":
+        setTelefono(value);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const Act = async (e) => {
     e.preventDefault();
 
-    // Verificar si alguno de los campos tiene espacios en blanco
-    if (
-      firstName.trim() === "" ||
-      lastName.trim() === "" ||
-      email.trim() === "" ||
-      telefono.trim() === ""
-    ) {
+    // Verificar si alguno de los campos está vacío
+    if (!firstName || !lastName || !email || !telefono) {
       swal({
         title: "Por favor, complete todos los campos",
         icon: "error",
@@ -39,37 +68,39 @@ const Editprofile = () => {
       const req = await axios.patch(
         `${VITE_url_Backend}/editProfile`,
         {
-            correo: email,
-            nombre: firstName,
-            apellido: lastName,
-            telefono: telefono
-        },{
-            headers: {
-                accessToken: localStorage.getItem("accessToken")
-            }
-        })
-        if (e) {
-            swal({
-                title: "¡Datos Actualizados!, ¡Es necesario volver a iniciar sesión!",
-                buttons: "Volver a iniciar sesión",
-                }).then((confirm) => {
-                if (confirm) {
-                    window.location.href = `${VITE_url_fronten}/login`;
-                }
-                });
-            }
-        console.log(req);
-    } catch ( error ) {
-        console.log( error );
-        alert( "Usuario no encontrado" )
+          correo: email,
+          nombre: firstName,
+          apellido: lastName,
+          telefono: telefono,
+        },
+        {
+          headers: {
+            accessToken: localStorage.getItem("accessToken"),
+          },
+        }
+      );
+      if (req) {
+        swal({
+          title: "¡Datos Actualizados!, ¡Es necesario volver a iniciar sesión!",
+          buttons: "Volver a iniciar sesión",
+        }).then((confirm) => {
+          if (confirm) {
+            window.location.href = `${VITE_url_fronten}/login`;
+          }
+        });
+      }
+      console.log(req);
+    } catch (error) {
+      console.log(error);
+      alert("Usuario no encontrado");
     }
   };
 
   let navigate = useNavigate();
 
-    useEffect(() => {
-    if (localStorage.getItem( "accessToken" )) {
-        setLoading( false );
+  useEffect(() => {
+    if (localStorage.getItem("accessToken")) {
+      setLoading(false);
     } else {
       navigate("/");
     }
@@ -82,46 +113,71 @@ const Editprofile = () => {
     } catch (error) {
       alert(error);
     }
-    };
-    return (
-        <Maincontainer style={{ display: "flex", alignItems: "center" }}>
-        <Header>
-            <FiChevronLeft onClick={ ret } className="volver" />
-            <Tittle1 src={ Wanto } alt="Wanto" />
-            <FiChevronLeft className="invisible"/>
-        </Header>
-        <ContainerBody>
-            <InfoUserContainer >
-            <Tiltle>
-            <Containerimg src={ Perfilfinal } alt="perfilfinal" />
-            </Tiltle>
+  };
+  
+  return (
+    <Maincontainer style={{ display: "flex", alignItems: "center" }}>
+      <Header>
+        <FiChevronLeft onClick={ret} className="volver" />
+        <Tittle1 src={Wanto} alt="Wanto" />
+        <FiChevronLeft className="invisible" />
+      </Header>
+      <ContainerBody>
+        <InfoUserContainer>
+          <Tiltle>
+            <Containerimg src={Perfilfinal} alt="perfilfinal" />
+          </Tiltle>
 
           <FullName>
             <div>
-                <input type="text" placeholder="First_Name" onChange={( e ) =>setFirtsName( e.target.value )}  />
+              <input
+                type="text"
+                name="firstName"
+                placeholder="First_Name"
+                value={firstName}
+                onChange={handleInputChange}
+              />
             </div>
 
             <div>
-                <input type="text" placeholder="Last_Name" onChange={(e)=>setLastName( e.target.value )}/>
+              <input
+                type="text"
+                name="lastName"
+                placeholder="Last_Name"
+                value={lastName}
+                onChange={handleInputChange}
+              />
             </div>
           </FullName>
 
-            <InforUser>
+          <InforUser>
             <div className="Inputs">
-                <input type="text" placeholder="Email" onChange={( e ) =>setEmail( e.target.value )}  />
+              <input
+                type="text"
+                name="email"
+                placeholder="Email"
+                value={email}
+                onChange={handleInputChange}
+              />
             </div>
             <div className="Inputs">
-                <input type="text" placeholder="Contact_Number" onChange={(e)=>setTelefono( e.target.value )}  />
+              <input
+                type="text"
+                name="telefono"
+                placeholder="Contact_Number"
+                value={telefono}
+                onChange={handleInputChange}
+              />
             </div>
-            </InforUser>
-            <div className="botones">
-                <ButtUser onClick={ Act }>Save</ButtUser>
-                <ButtUser onClick={ ret }>Cancel </ButtUser>
-            </div>
-            </InfoUserContainer>
-        </ContainerBody>
-        </Maincontainer>
-    );
+          </InforUser>
+          <div className="botones">
+            <ButtUser onClick={Act}>Save</ButtUser>
+            <ButtUser onClick={ret}>Cancel </ButtUser>
+          </div>
+        </InfoUserContainer>
+      </ContainerBody>
+    </Maincontainer>
+  );
 };
 
 export default Editprofile;
