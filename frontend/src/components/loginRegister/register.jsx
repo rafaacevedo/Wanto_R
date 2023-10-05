d93caf8cf4812e5a88dcc9d89a34c4cecebb0af2
alejandro brinez <abrinez@ulearning.com.co>
2023-10-05 09:10:03 -0500

    cambios

--- frontend/src/components/loginRegister/register.jsx
+++ frontend/src/components/loginRegister/register.jsx
@@ -9,45 +9,46 @@ import {
   Principal,
   Wanto,
 } from "./styles/styledRegister";
-
 import wanto from "../asset/Wanto.svg";
 import { FiChevronLeft } from "react-icons/fi";
 import { useState } from "react";
-import {  VITE_url_Backend,VITE_url_fronten } from "../home/home";
+import { VITE_url_Backend, VITE_url_fronten } from "../home/home";
+
 const Register = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [name, setFirstName] = useState("");
-  const [lastname, setLastName] = useState("");
-  const [number, setNumber] = useState("");
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+    name: "",
+    lastname: "",
+    number: "",
+  });
+
+  const { email, password, name, lastname, number } = formData;
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    // Eliminar espacios en blanco del valor ingresado
+    const trimmedValue = value.trim();
+
+    setFormData({
+      ...formData,
+      [name]: trimmedValue,
+    });
+  };
 
   const handleSubmit = async (e) => {
-    e.preventDefault()
-    if (
-      email === "" ||
-      password === "" ||
-      name === "" ||
-      lastname === "" ||
-      number === ""
-    ) {
+    e.preventDefault();
+
+    if (!email || !password || !name || !lastname || !number) {
       swal({
-        title: "Enter information in the form",
+        title: "Please fill out all fields",
         icon: "error",
         button: "Try again",
       });
       return;
     }
-    else {
-      swal({
-        title: "correctly stored information",
-        icon: "success",
-        button: "back to login",
-      }).then((confirm) => {
-        if (confirm) {
-          window.location.href = `${VITE_url_fronten}/login`;
-        }
-      });
-    }
+
     try {
       const response = await axios.post(`${VITE_url_Backend}/register`, {
         correo: email,
@@ -57,23 +58,35 @@ const Register = () => {
         telefono: number,
       });
       console.log(response.data);
+
+      swal({
+        title: "Successfully registered",
+        icon: "success",
+        button: "Back to login",
+      }).then((confirm) => {
+        if (confirm) {
+          window.location.href = `${VITE_url_fronten}/login`;
+        }
+      });
     } catch (error) {
       console.error(error);
+      swal({
+        title: "Registration failed",
+        text: "An error occurred while registering.",
+        icon: "error",
+        button: "OK",
+      });
     }
   };
 
-  const cone = () => {
-    try {
-      window.location.href = `${VITE_url_fronten}/login`;
-    } catch (error) {
-      alert(error);
-    }
+  const redirectToLogin = () => {
+    window.location.href = `${VITE_url_fronten}/login`;
   };
 
   return (
     <Principal>
       <Bienvenido>
-        <FiChevronLeft onClick={cone} className="atras" />
+        <FiChevronLeft onClick={redirectToLogin} className="atras" />
       </Bienvenido>
       <Wanto>
         <Imgwanto src={wanto} alt="wanto" />
@@ -82,40 +95,49 @@ const Register = () => {
         <form>
           <Input
             type="text"
+            name="name"
             placeholder="First Name"
             value={name}
-            onChange={(e) => setFirstName(e.target.value)}
+            onChange={handleChange}
             required
           />
           <Input
             type="text"
+            name="lastname"
             placeholder="Last Name"
             value={lastname}
-            onChange={(e) => setLastName(e.target.value)}
+            onChange={handleChange}
             required
           />
           <Input
             type="text"
+            name="number"
             placeholder="Phone Number"
             value={number}
-            onChange={(e) => setNumber(e.target.value.replace(/[^0-9]/g, ""))}
+            onChange={handleChange}
             required
           />
           <Input
             type="email"
+            name="email"
             placeholder="Email address"
             value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            onChange={handleChange}
             required
           />
           <Input
             type="password"
+            name="password"
             placeholder="Password"
             value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            onChange={handleChange}
             required
           />
-          <Input type="password" placeholder="Confirm Password" required />
+          <Input
+            type="password"
+            placeholder="Confirm Password"
+            required
+          />
           <Boton onClick={handleSubmit}>
             <h2>Sign Up</h2>
           </Boton>
--- frontend/src/components/profile/edit_profile/edit_profile.jsx
+++ frontend/src/components/profile/edit_profile/edit_profile.jsx
@@ -1,113 +1,155 @@
-import { Header, Maincontainer} from "../styledProfile";
-import { useEffect, useState }  from "react";
-import { useNavigate }          from 'react-router-dom'
-import { ContainerBody, Tittle1 , Tiltle, FullName, InforUser, Containerimg, ButtUser, InfoUserContainer }   from "./styledEdit_profile";
-import  Wanto                   from "../../asset/Wanto.svg";
-import  Perfilfinal             from "../../asset/perfilfinal.png";
-import { FiChevronLeft }        from "react-icons/fi";
-import { VITE_url_fronten, VITE_url_Backend  }     from "../../home/home";
+import { Header, Maincontainer } from "../styledProfile";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import {
+  ContainerBody,
+  Tittle1,
+  Tiltle,
+  FullName,
+  InforUser,
+  Containerimg,
+  ButtUser,
+  InfoUserContainer,
+} from "./styledEdit_profile";
+import Wanto from "../../asset/Wanto.svg";
+import Perfilfinal from "../../asset/perfilfinal.png";
+import { FiChevronLeft } from "react-icons/fi";
+import { VITE_url_fronten, VITE_url_Backend } from "../../home/home";
 import axios from "axios";
 import swal from "sweetalert";
+
 const Editprofile = () => {
+  const [, setLoading] = useState(true);
+  const [firstName, setFirtsName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [telefono, setTelefono] = useState("");
 
-    const [, setLoading ] = useState( true );
-    const [firstName, setFirtsName] = useState( '' );
-    const [lastName, setLastName] = useState  ( '' );
-    const [email, setEmail] = useState        ( '' );
-    const [telefono, setTelefono] = useState  ( '' );
-    
-
-    const Act =  async ( e ) => {
+  const Act = async (e) => {
     e.preventDefault();
+
+    // Verificar si alguno de los campos tiene espacios en blanco
+    if (
+      firstName.trim() === "" ||
+      lastName.trim() === "" ||
+      email.trim() === "" ||
+      telefono.trim() === ""
+    ) {
+      swal({
+        title: "Por favor, complete todos los campos",
+        icon: "error",
+        button: "Intentar de nuevo",
+      });
+      return;
+    }
+
     try {
-            const req = await axios.patch(`${ VITE_url_Backend }/editProfile`,
+      const req = await axios.patch(
+        `${VITE_url_Backend}/editProfile`,
         {
-            correo: email,
-            nombre: firstName,
-            apellido: lastName,
-            telefono: telefono
-        },{
-            headers: {
-                accessToken: localStorage.getItem("accessToken")
-            }
-        })
-        if (e) {
-            swal({
-                title: "¡Datos Actualizados!, ¡Es necesario volver a iniciar sesión!",
-                buttons: "Volver a iniciar sesión",
-                }).then((confirm) => {
-                if (confirm) {
-                    window.location.href = `${VITE_url_fronten}/login`;
-                }
-                });
-            }
-        console.log(req);
-    } catch ( error ) {
-        console.log( error );
-        alert( "Usuario no encontrado" )
+          correo: email,
+          nombre: firstName,
+          apellido: lastName,
+          telefono: telefono,
+        },
+        {
+          headers: {
+            accessToken: localStorage.getItem("accessToken"),
+          },
+        }
+      );
+      if (e) {
+        swal({
+          title:
+            "¡Datos Actualizados! Es necesario volver a iniciar sesión",
+          buttons: "Volver a iniciar sesión",
+        }).then((confirm) => {
+          if (confirm) {
+            window.location.href = `${VITE_url_fronten}/login`;
+          }
+        });
+      }
+      console.log(req);
+    } catch (error) {
+      console.log(error);
+      alert("Usuario no encontrado");
     }
-    }
+  };
 
+  let navigate = useNavigate();
 
-    let navigate = useNavigate();
-
-    useEffect(() => {
-    if (localStorage.getItem( "accessToken" )) {
-        setLoading( false );
+  useEffect(() => {
+    if (localStorage.getItem("accessToken")) {
+      setLoading(false);
     } else {
-        navigate( "/" );
+      navigate("/");
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+  }, []);
 
-    
-const ret = () => {
-    
+  const ret = () => {
     try {
-        window.location.href = `${ VITE_url_fronten }/profile`;
-    } catch ( error ) {
-        alert( error );
+      window.location.href = `${VITE_url_fronten}/profile`;
+    } catch (error) {
+      alert(error);
     }
-    };
-    return (
-        <Maincontainer style={{ display: "flex", alignItems: "center" }}>
-        <Header>
-            <FiChevronLeft onClick={ ret } className="volver" />
-            <Tittle1 src={ Wanto } alt="Wanto" />
-            <FiChevronLeft className="invisible"/>
-        </Header>
-        <ContainerBody>
-            <InfoUserContainer >
-            <Tiltle>
-            <Containerimg src={ Perfilfinal } alt="perfilfinal" />
-            </Tiltle>
+  };
 
-            <FullName>
+  return (
+    <Maincontainer style={{ display: "flex", alignItems: "center" }}>
+      <Header>
+        <FiChevronLeft onClick={ret} className="volver" />
+        <Tittle1 src={Wanto} alt="Wanto" />
+        <FiChevronLeft className="invisible" />
+      </Header>
+      <ContainerBody>
+        <InfoUserContainer>
+          <Tiltle>
+            <Containerimg src={Perfilfinal} alt="perfilfinal" />
+          </Tiltle>
+
+          <FullName>
             <div>
-                <input type="text" placeholder="First_Name" onChange={( e ) =>setFirtsName( e.target.value )}  />
+              <input
+                type="text"
+                placeholder="First_Name"
+                onChange={(e) => setFirtsName(e.target.value)}
+              />
             </div>
 
             <div>
-                <input type="text" placeholder="Last_Name" onChange={(e)=>setLastName( e.target.value )}/>
+              <input
+                type="text"
+                placeholder="Last_Name"
+                onChange={(e) => setLastName(e.target.value)}
+              />
             </div>
-            </FullName>
+          </FullName>
 
-            <InforUser>
+          <InforUser>
             <div className="Inputs">
-                <input type="text" placeholder="Email" onChange={( e ) =>setEmail( e.target.value )}  />
+              <input
+                type="text"
+                placeholder="Email"
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </div>
             <div className="Inputs">
-                <input type="text" placeholder="Contact_Number" onChange={(e)=>setTelefono( e.target.value )}  />
+              <input
+                type="text"
+                placeholder="Contact_Number"
+                onChange={(e) => setTelefono(e.target.value)}
+              />
             </div>
-            </InforUser>
-            <div className="botones">
-                <ButtUser onClick={ Act }>Save</ButtUser>
-                <ButtUser onClick={ ret }>Cancel </ButtUser>
-            </div>
-            </InfoUserContainer>
-        </ContainerBody>
-        </Maincontainer>
-    );
+          </InforUser>
+          <div className="botones">
+            <ButtUser onClick={Act}>Guardar</ButtUser>
+            <ButtUser onClick={ret}>Cancelar</ButtUser>
+          </div>
+        </InfoUserContainer>
+      </ContainerBody>
+    </Maincontainer>
+  );
 };
 
 export default Editprofile;
