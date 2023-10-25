2dd67ded7a64f23a56604fee0da02fe267b339ed
alejandro brinez <abrinez@ulearning.com.co>
2023-10-25 15:06:31 -0500

    Cambios en home de usuario

--- frontend/src/components/home-client/HomeClient.jsx
+++ frontend/src/components/home-client/HomeClient.jsx
@@ -1,15 +1,29 @@
+/* eslint-disable no-unused-vars */
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import jwt_decode from 'jwt-decode';
 import { VITE_url_Backend, VITE_url_fronten } from "../home/home";
-import { Container, Header, Invisible, Principal } from './StyledHomeClients';
+import { Cartas, Container, ContenedorBotones, Header, HeaderCartas, Iconos, Info, Invisible, Principal } from './StyledHomeClients';
 import {BiSolidUserCircle} from 'react-icons/bi'
 import {SlLogout} from 'react-icons/sl'
 import swal from "sweetalert";
+import { Spinner } from 'reactstrap';
+import { BsDisplay } from "react-icons/bs";
+import { RiRestartFill } from "react-icons/ri";
+import { BsFillPlayCircleFill } from "react-icons/bs";
+import { HiStop } from "react-icons/hi";
+import { BsToggle2Off } from "react-icons/bs";
+import { RiInstallFill } from "react-icons/ri";
+import { PiComputerTowerFill } from "react-icons/pi";
 
 
 
 const HomeClient = () => {
+  const [data, setData] = useState([]);
+  const [/* error */, setError] = useState();
+
+  const [, setLoading] = useState(true);
+
   const [machine, setMachine] = useState(null);
 
   const token = localStorage.getItem('accessToken');
@@ -42,6 +56,103 @@ const HomeClient = () => {
     obtenerMaquinaPorUsuario(idUsuario); 
   }
 }, [idUsuario]);
+
+async function fetchData() {
+  try {
+    const response = await axios.get(`${VITE_url_Backend}/api/data`);
+
+    if (Array.isArray(response.data.data)) {
+      // Verifica si los datos son un array
+      setData(response.data.data);  
+    } else {
+      // Los datos no son un array válido, maneja este caso según tus necesidades
+      setError("Los datos no son un array válido");
+    }
+
+    setLoading(false);
+  } catch (error) {
+    setError(error.message);
+    setLoading(false);
+  }
+}
+
+useEffect(() => {
+  fetchData();
+
+  setInterval(() => {
+    fetchData();
+  }, 1000);
+
+
+}, [setLoading, setError]);
+
+
+
+const [status, setStatus] = useState('');
+
+const handleStopButtonClick = async (status) => {
+  if (status === "stopped") {
+    return "Error"
+  }
+  else {
+  try {
+    const response = await axios.post(`${VITE_url_Backend}/api/stop`);
+    setStatus('Máquina detenida con éxito.');
+  } catch (error) {
+        setStatus('La maquina ya está detenida.');
+        setError(error)
+        console.error(error);
+      }
+  }
+};
+
+const handleStartButtonClick = async (status) => {
+  if (status === "running") {
+    return "Error"
+  }
+  else {
+      try {
+        // eslint-disable-next-line no-unused-vars
+        const response = await axios.post(`${VITE_url_Backend}/api/start`);
+        setStatus('Máquina encendida con éxito.');
+      } catch (error) {
+        setStatus('La maquina ya está encendida.');
+        setError(error)
+        console.error(error);
+      }  
+  }
+};
+
+const handleRestartButtonClick = async () => {
+  try {
+    const response = await axios.post(`${VITE_url_Backend}/api/restart`);
+    setStatus('Máquina reiniciada con éxito.');
+  } catch (error) {
+    setStatus('Error al reiniciar la máquina.');
+    setError(error)
+    console.error(error);
+  }
+};
+
+const handleshutdowntButtonClick = async () => {
+  if (status === "stopped") {
+    return "Error"
+  }
+  else {
+  try {
+    const response = await axios.post(`${VITE_url_Backend}/api/shutdown`);
+    setStatus('Apagando la maquina.');
+    setRotate(!rotate)
+  } catch (error) {
+    setStatus('La maquina ya está apagada');
+    setError(error)
+    console.error(error);
+  }
+  }
+};
+
+
+const [rotate, setRotate] = useState(false)
 
 if (!machine) {
     return(
@@ -95,11 +206,101 @@ if (!machine) {
       </Header>
           <Invisible/>
           <Container>
-            <div>
+          {data.length > 0 ? (
+          <>
+            {data.map((instance) => (
+              <Cartas key={instance.instanceId}>
+                <HeaderCartas>
+
+                    <div key={instance.instanceId} className="titulo">
+                      <h1 className="titulo">{instance.ipConfig.v4.ip}</h1>
+                    </div>
+
+                </HeaderCartas>
+                <Iconos>
+                  <ContenedorBotones rotate = {rotate}>
+                    
+                    <BsDisplay className="Status" title= {instance.status} style={instance.status === "running" ? { fill: "#068EEB" } : { fill: "grey" }} />
+
+                    <BsFillPlayCircleFill className="Start" title='Start' onClick={() => handleStartButtonClick(instance.status)} />
+
+                    <RiRestartFill className="Restart" title='Restart' onClick={handleRestartButtonClick}/>
+                    
+                    <HiStop className="Stop" title='Stop' onClick={() => handleStopButtonClick(instance.status)} />
+                    
+                    <BsToggle2Off className="Cloud-Init" title='Cloud-Init' onClick={() => handleshutdowntButtonClick(instance.status)}/>
+                    
+                    <RiInstallFill className="Reinstall" title='Reinstall' onClick={() => {
+                      // eslint-disable-next-line no-constant-condition
+                      if (true) {
+                        swal({
+                          title: "Button disabled for now",
+                          icon: "error",
+                          button: "Return",
+                        }).then((confirm) => {
+                          if (confirm) {
+                            window.location.href = `${VITE_url_fronten}/homeclient`;
+                          }
+                        });
+                      }
+                    }}/>
+                  
+                  </ContenedorBotones>
+                    <PiComputerTowerFill className="pc" style={instance.status === "running" ? { fill: "#5C8E24" } : { fill: "grey" }} />
+                    
+                </Iconos>
+                <Info>
+                    <div key={instance.instanceId} className="info">
+                      <h2>Información:</h2>
+                      <Info>
+                        <h3>Nombre: </h3>
+                        <p>{instance.name}</p>
+                      </Info>
+                      <Info>
+                        <h3>OsType: </h3>
+                        <p>{instance.osType}</p>
+                      </Info>
+                      <Info>
+                        <h3>Instancia Id: </h3>
+                        <p>{instance.instanceId}</p>
+                      </Info>
+                      <Info>
+                        <h3>Region: </h3>
+                        <p>{instance.regionName}</p>
+                      </Info>
+                      <Info>
+                        <h3>RamMb: </h3>
+                        <p>{instance.ramMb}</p>
+                      </Info>
+                      <Info>
+                        <h3>Instancia Ip: </h3>
+                        <p>{instance.ipConfig.v4.ip}</p>
+                      </Info>
+                      <Info>
+                        <h3>Status: </h3>
+                        <p>{instance.status}</p>
+                      </Info>
+                      <Info>
+                        <h3 className='botonesStatus' style={status.includes("está") ? { color: "red" } : { color: "#247e8e" }} >{status}</h3>
+                      </Info>
+                    </div>
+                  
+                </Info>
+              </Cartas>
+            ))}
+          </>
+        ) : (
+          <>
+            <div className="loading">
+              <Spinner className="spinner" />
+            </div>
+          </>
+        )}
+            {/* <div>
               <h1>Información de la máquina</h1>
               <p>ID de la máquina: {machine.id_maquina}</p>
               <p>Instancia: {machine.instancia}</p>
-            </div>
+            </div> */}
           </Container>
     </Principal>
   );
--- frontend/src/components/home-client/StyledHomeClients.jsx
+++ frontend/src/components/home-client/StyledHomeClients.jsx
@@ -70,3 +70,250 @@ export const Container = styled.div`
     align-items: center;
     /* background-color: green; */
 `
+
+export const Cartas = styled.div`
+    width: 20%;
+    height: 100%;
+    margin: 10px;
+    display: flex;
+    flex-direction: column;
+    border-radius: 10px;
+    background-color: #fff;
+    border: 1px solid grey;
+    h1{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    .pc{
+        margin-top: 40px;
+        font-size: 200px;
+        color: silver;
+    }
+    h2{
+        margin: 30px 0px 20px 20px;
+    }
+    .info{
+        height: auto;
+        width: 100%;
+        margin-top: 10px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+    }
+    .Carta_infor{
+        height: auto;
+        width: 100%;
+        justify-content: center;
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+    }
+    li{
+        margin-left: 20px;
+    }
+    @media (min-width: 280px) and (max-height: 1000px) {
+    width: 85%;
+    height: auto;
+    margin: auto;
+    display: flex;
+    flex-direction: column;
+    border-radius: 10px;
+    border: 1px solid grey;
+    h1{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    .pc{
+        margin-top: 40px;
+        font-size: 200px;
+        color: silver;
+    }
+    h2{
+        margin: 30px 0px 20px 20px;
+    }
+    .info{
+        height: 100%;
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+    }
+    .Carta_infor{
+        width: 100%;
+        justify-content: center;
+        display: flex;
+        align-items: center;
+        flex-direction: column  ;
+    }
+    li{
+        margin-left: 20px;
+    }
+}
+@media (min-height: 100px){
+    width: 85%;
+    height: auto;
+    margin: auto;
+    display: flex;
+    flex-direction: column;
+    border-radius: 10px;
+    border: 1px solid grey;
+    h1{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    .pc{
+        margin-top: 40px;
+        font-size: 200px;
+        color: silver;
+    }
+    h2{
+        margin: 30px 0px 20px 20px;
+    }
+    .info{
+        height: 100%;
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+    }
+    .Carta_infor{
+        width: 100%;
+        justify-content: center;
+        display: flex;
+        align-items: center;
+        flex-direction: column  ;
+    }
+    li{
+        margin-left: 20px;
+    }
+}
+@media (min-width: 500px){
+    width: 400px;
+}
+`
+
+export const HeaderCartas = styled.div`
+    height: 13;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 20px 20px 0px 0px;
+    border-bottom: 1px solid grey;
+        h1{
+            color: #201B6F;
+            font-size: 40px;
+            font-family: 'Poppins', sans-serif;
+            font-weight: 600;
+        }
+`
+
+export const ContenedorBotones = styled.div`
+    width: 25%;
+    height: 200px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin: 40px 0px 0px 70px;
+    flex-wrap: wrap;
+    
+    svg{
+        margin-bottom: 10px;
+        font-size: 40px;
+        cursor: pointer;
+    }
+    .Status{
+        color: #068EEB;
+        cursor: auto;
+        &:hover{
+            color: #068EEB;
+        }
+    }
+    .Restart{
+        color: #153A6F;
+        &:hover{
+            color: #2a5ea7;
+        }
+    }
+    .Start{
+        color: #5C8E24;
+        font-size: 35px;
+        &:hover{
+            color: #80c234;
+        }
+    }
+    .Stop{
+        color: #BC6228;
+        &:hover{
+            color: #f08741;
+        }
+    }
+    .Cloud-Init{
+        color: #be1b09;
+        transform: ${({rotate}) => (rotate ? 'rotate(0deg)' : 'rotate(180deg)')};
+        color: ${({rotate}) => (rotate ? 'grey' : '#be1b09')};
+    }
+    .Reinstall{
+        color: #708DA3;
+        &:hover{
+            color: #95bbd8;
+        }
+    }
+    .Rescue{
+        color: #475c7f;
+        &:hover{
+            color: #617eb1;
+        }
+    }
+    .Snap-Shots{
+        color: #767678;
+        &:hover{
+            color: #afafb8;
+        }
+    }
+    .Arrow{
+        color: #068EEB;
+    }
+    @media (min-width: 375px) and (max-height: 856px) {
+    width: 25%;
+    height: 50%;
+    display: flex;
+    align-content: center;
+    justify-content: center;
+    margin: auto;
+    flex-wrap: wrap;
+    }
+`
+
+export const Info = styled.div`
+    height: auto;
+    width: 100%;
+    display: flex;
+    margin-top: -10px;
+    h3{
+        margin-left: 20px;
+        margin-right: 10px;
+        font-size: 20px;
+        font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+    }
+    p{
+        font-family: Arial, Helvetica, sans-serif;
+        color: #201B6F;
+        font-weight: 900;
+    }
+    h2{
+        font-weight: 600;
+    }
+    .botonesStatus{
+        color: #068EEB;
+    }
+`
+
+export const Iconos = styled.div`
+    height: 50%;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    flex-direction: row;
+`
